46401991abd0c0a29b5a14327c8e860932bf623f
Bryan Segovia <bryan.segovia@tecsup.edu.pe>
2024-09-29 10:38:44 -0500

    main antes de la tarea

--- app/src/main/java/com/example/lab08/TaskDao.kt
+++ app/src/main/java/com/example/lab08/TaskDao.kt
@@ -9,19 +9,26 @@ import androidx.room.Update
 @Dao
 interface TaskDao {
 
+
     // Obtener todas las tareas
     @Query("SELECT * FROM tasks")
     suspend fun getAllTasks(): List<Task>
+
 
     // Insertar una nueva tarea
     @Insert
     suspend fun insertTask(task: Task)
 
+
     // Marcar una tarea como completada o no completada
     @Update
     suspend fun updateTask(task: Task)
+
 
     // Eliminar todas las tareas
     @Query("DELETE FROM tasks")
     suspend fun deleteAllTasks()
 }
+
+
+
--- app/src/main/java/com/example/lab08/TaskViewModel.kt
+++ app/src/main/java/com/example/lab08/TaskViewModel.kt
@@ -51,3 +51,4 @@ class TaskViewModel(private val dao: TaskDao) : ViewModel() {
         }
     }
 }
+
